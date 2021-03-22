9e462dcfc8dadbca36ee5bf1548440356d3d43ea
someKidNamedQuinn <66712499+someKidNamedQuinn@users.noreply.github.com>
2021-03-22 16:38:42 -0600

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -11,4 +11,45 @@ import javax.swing.JComponent;
  */
 public class GamePanel extends JComponent {
 
+  private int myTimerDelay;
+	private Timer myTimer;
+	private Image img;
+	private int scrolling;
+	
+	public GamePanel() {
+		
+		//working to add background and scroll it
+		scrolling = 400;
+		ImageIcon i = new ImageIcon("background.png");
+		img = i.getImage();
+		
+		
+		//timer stuff still kinda wack
+		myTimerDelay = 10;
+		TimerTask gameTimer = new TimerTask() {
+			@Override
+			public void run() {
+				//replace to updastae sprites every whenever
+				//System.out.println("test");
+				repaint();
+				scrolling += 50;
+				//keep this but tweak 
+				if(scrolling > 750) {
+					scrolling = -200;
+				}
+				
+			}
+		};
+		Timer timer = new Timer();
+		timer.schedule(gameTimer, 1000, 1000);
+		
+	}
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D graphics = (Graphics2D) g;
+		graphics.drawImage(img, scrolling,100, null);
+	}
+	
+  
 }
