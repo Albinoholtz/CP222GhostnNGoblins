14ec0943d04f06727aecbab7ab22113fe68c7308
someKidNamedQuinn <66712499+someKidNamedQuinn@users.noreply.github.com>
2021-03-23 12:04:46 -0600

    Update GameRunner.java

--- GameRunner.java
+++ GameRunner.java
@@ -1,5 +1,11 @@
-package GhostAndGoblins;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 /**
@@ -12,34 +18,8 @@ public class GameRunner {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		GameRunner game = new GameRunner(); // creates game instance
+		GamePanel game = new GamePanel();
 		game.run(); // runs game
 
 	}
-	
-	/**
-	 * runs the game ghosts and goblins
-	 * @throws IOException 
-	 */
-	public void run(){ 
-		JFrame mainWin = new JFrame("Ghosts n' Goblins"); // creates window
-		
-		mainWin.setSize(750, 522); // sizes window
-		
-		GamePanel mainPanel = new GamePanel(); // creates game component
-		InputListener listener = new InputListener(mainPanel); // creates game listener
-		
-		mainWin.add(mainPanel); // adds game component to main window
-		mainWin.getContentPane().setBackground(Color.black);
-		
-		//ImageIcon img = new ImageIcon("background.png");
-		//paintBackground()
-		//mainWin.getContentPane().setB
-					//ImageIO.read(new File("c:\\background.png"));
-		
-		mainWin.setVisible(true); // shows game
-		
-		mainWin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
 }
