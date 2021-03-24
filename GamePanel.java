d1e01acadd7dc6568deedd9df49dd2153c05e1bf
someKidNamedQuinn <66712499+someKidNamedQuinn@users.noreply.github.com>
2021-03-24 09:20:52 -0600

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,194 +1,328 @@
 
-import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
 /**
  * @author William & Quinn
  *
  */
-public class Zombie extends Enemy {
+public class GamePanel extends JPanel{
 	
-	BufferedImage[] spawnImg = new BufferedImage[3];
-	BufferedImage[] walkImg = new BufferedImage[3];
-	BufferedImage[] dyingImg = new BufferedImage[2];
-	BufferedImage currentImage = null;
-	String state = null;
-	double spawnCount = -0.5;
-	double dyingCount = -0.5;
-	int velocity = 0;
-	String direction = "right";
+	private int jumpCount = 0;
+	private int jumpVelocity = 0;
+	private int fps = 60;
+	private int distance = 0;
+	private int heroSpeed = 4;
+	private Image backgroundImg;
+	private Image scoreImg;
+	private InputListener listener;
+	private int cooldown = 0;
+	private boolean isRunning = true;
+	private boolean jumping = false;
+	private boolean falling = false;
+	private int scrolling;
+	private Knight knight;
+	private boolean hurt = false;
+	Clip startSound;
+	private ArrayList<Sprite> spriteList = new ArrayList<Sprite>();
+	ArrayList<Structure> groundArr = new ArrayList<Structure>();
+
+	public GamePanel() {
+		
+		try {
+			startSound = playSound("GAMESTART.wav");
+		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	
-	/**
-	 * @param path
-	 * @param _x
-	 * @param _y
-	 */
-	public Zombie(int _x, int _y) {
-		super(_x, _y);
-		
-		BufferedImage spriteSheet = getImageFile("enemies.png"); // load file
-		BufferedImage[] allImg = new BufferedImage[6]; // create list for cutting images
-
-
-		for (int i = 0; i < 6; i++) { // cut images and add to master list
-			if (i < 3) {
-				allImg[i] = spriteSheet.getSubimage(i * 22, 66, 24, 32);
-			} else {
-			allImg[i] = spriteSheet.getSubimage((i * 25) - 6, 66, 24, 32); 
-			}
-		}
-		
-		for (int i = 0; i < allImg.length; i++) { // add images to proper lists
-			if (i < 3) {
-				walkImg[i] = allImg[i];
-			} else {
-				spawnImg[i - 3] = allImg[i];
-			}
-		}
-		
-		Image spriteSheetImg = (Image) getImageFile("knight.png");
-		
-		Color backPink = new Color(252,105,216,255);
-		Color backBlue = new Color(117,146,252,255);
-		Color backGreen = new Color(167,226,110,255);
-		Color backCyan = new Color(95,205,228,255);
-		
-		spriteSheetImg = Transparency.makeColorTransparent(spriteSheetImg, backBlue);
-		spriteSheetImg = Transparency.makeColorTransparent(spriteSheetImg, backPink);
-		spriteSheetImg = Transparency.makeColorTransparent(spriteSheetImg, backGreen);
-		spriteSheetImg = Transparency.makeColorTransparent(spriteSheetImg, backCyan);
-		
-		spriteSheet = toBufferedImage(spriteSheetImg);
-		dyingImg[0] = spriteSheet.getSubimage(192, 128, 32, 32);
-		dyingImg[1] = spriteSheet.getSubimage(224, 128, 32, 32);
-		
-		if (getX() < 320) {
-			direction = "right";
-		} else {
-			direction = "left";
-		}
-		
-		state = "spawing";
-		currentImage = spawnImg[2];
-		setImage(currentImage);
-
-	}
-
-	@Override
-	public boolean collidedWith(Sprite other) {
-		boolean collided = false;
-		if (overlaps(other)) {
-			if (other instanceof Weapon && state != "dying") { // After collision with weapon, die
-				try {
-					playSound("ENEMYDEATH.wav");
-				} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				state = "dying";
-			}
-		}
-		return collided;
-	}
-
-	@Override
-	public void update(int time) {
-		
-		switch (state) {
-		case "spawing":
-			spawnCount += 0.1;
-			
-			if ((int) (spawnCount / 4) >= 3) {
-				state = "walking";
-				setImage(walkImg[time % 3]);
-				if (direction == "right") { // set starting direction
-					velocity = 4;
-				} else {
-					velocity = -4;
-				}
-				break;
-			}
-			setImage(spawnImg[2 - (int) (spawnCount / 4)]);
-			break;
-		case "walking":
-			setImage(walkImg[time % 3]);
-			if (getX() < -10) { // switch direction
-				velocity = 4;
-				direction = "right";
-			} else if (getX() > 680){ // switch direction
-				velocity = -4;
-				direction = "left";
-			}
-			
-			updateX(velocity);
-			break;
-		case "dying":
-			dyingCount += 0.2;
-			
-			if ((int) (dyingCount) >= 2) {
-				setRemove(true);
-				break;
-			}
-			
-			setImage(dyingImg[(int) (dyingCount)]);
-			break;
+		//adding initial ground
+		int xCor = -92;
+		for(int i = 0; i < 20; i++) {
+			Structure ground = new Structure(xCor, 580);
+			groundArr.add(ground);
+			xCor += 46;
 			
 		}	
 	}
 	
-	void draw(Graphics2D g2) {
+	public void checkCollisions(ArrayList<Sprite> spriteList) {
+    	ArrayList<Sprite> removeIndex = new ArrayList<Sprite>();
+    	
+    	for (int i = 0; i < spriteList.size(); i++) {
+    		for (int j = 0; j < spriteList.size(); j++) {
+    			if (i != j) {
+    				if (spriteList.get(i).collidedWith(spriteList.get(j))) {
+    					removeIndex.add(spriteList.get(i));
+    				}
+    			}
+    		}
+    	}
+	}
 
+	public void run() {
+		int updates = 0;
+		
+		//creating Knight
+		Knight knight = new Knight(320, 575);
+		this.knight = knight;
+		spriteList.add(knight);
 
-		// scales and draws the image by 2
-		if(direction == "right") {
-			g2.drawImage(getImage(), getX(), getY(), getImage().getWidth() * 3, getImage().getHeight() * 3, null);
+		//test zombie
+		Zombie myMan = new Zombie(100, 575);
+		spriteList.add(myMan);
+		//runs while the game is going
+		while(isRunning) {
+			
+			if(scrolling > 750) {
+				scrolling = -200;
+			}
+			long time = System.currentTimeMillis();
+
+			update(updates);
+			repaint();
+
+			time = (1000/fps) - (System.currentTimeMillis() - time);
+			if(time > 0) {
+				try {
+					Thread.sleep(time);
+				} catch (Exception e){}
+			}
+			
+			updates += 1;
+			if (updates > 600) {
+				updates = 0;
+			}
+		}
+		
+		Graphics g = this.getGraphics();
+		Graphics2D g2 = (Graphics2D) g;
+		
+		setVisible(false);
+		
+		ImageIcon tempImgOne = new ImageIcon("gameOver.png");
+		Image gameOverImg = tempImgOne.getImage();
+		g2.drawImage(gameOverImg, 225, 150, 250 , 250, null);
+		g2.drawImage(scoreImg, 225, 350, 100 , 100, null);
+		
+		
+		
+	}
+
+	public void initialize(){
+		//setting up all the stuffs
+		ImageIcon background = new ImageIcon("background.png");
+		backgroundImg = background.getImage();
+		
+		ImageIcon tempImgTwo = new ImageIcon("scoreText.png");
+		scoreImg = tempImgTwo.getImage();
+		
+		listener = new InputListener(this);
+		repaint();
+		
+	}
+	
+	public void update(int updates) {
+
+		if(jumping && this.knight.getState() != "runJump") {
+			this.knight.setState("stationaryJump");
+		} else if (jumping) {
+			this.knight.setState("runJump");
 		} else {
-			g2.drawImage(getImage(), getX() + 66, getY(), -(getImage().getWidth() * 3), getImage().getHeight() * 3, null);
+			this.knight.setState("idle");
+		}
+
+		if(listener.isKeyDown(KeyEvent.VK_DOWN)) {
+			this.knight.setState("crouch");
+		}
+		
+		if(listener.isKeyDown(KeyEvent.VK_RIGHT)) {
+			distance += 2;
+			
+			if(jumping) {
+				this.knight.setState("runJump");
+			} else {
+				this.knight.setState("running");
+			}
+			this.knight.setDirection("right");
+
+			// moves the sprites over based on player movement
+			for(int i = 0; i < groundArr.size(); i ++) {
+				groundArr.get(i).updateX(-heroSpeed);
+			}
+			for (int i = 0; i < spriteList.size(); i++) {
+				spriteList.get(i).updateX(-heroSpeed);
+			}	
+
+			// builds new ground
+			if(groundArr.get(groundArr.size()-1).getX() < 874){
+				int lastX = groundArr.get(groundArr.size()-1).getX();
+				Structure ground = new Structure(lastX + 46, 580);
+				groundArr.add(ground);
+			}
+
+
 		} 
 
-	}
-	
-	@Override
-	public Rectangle getHitbox() {
-		Rectangle hitbox = new Rectangle();
-		switch(state) {
-		case "walking":
-			hitbox.setSize(22, 32);
-			hitbox.setLocation(getX() + 1, getY());
-		default: // If dying or spawning, shouldn't hit
-			hitbox.setSize(22, 32);
-			hitbox.setLocation(getX() + 1, getY());
-			break;
+		if(listener.isKeyDown(KeyEvent.VK_LEFT)) {
+			distance -= 2;
+			if(jumping) {
+				this.knight.setState("runJump");
+			} else {
+				this.knight.setState("running");
+			}
+			this.knight.setDirection("left");
+
+			// moves the sprites over based on player movement
+			for(int i = 0; i < groundArr.size(); i ++) {
+				groundArr.get(i).updateX(heroSpeed);
+			}
+			for (int i = 0; i < spriteList.size(); i++) {
+				spriteList.get(i).updateX(heroSpeed);
+			}
+
+			// builds new ground
+			if(groundArr.get(0).getX() > -138) {
+				int firstX = groundArr.get(0).getX();
+				Structure ground = new Structure(firstX - 46, 580);
+				groundArr.add(0, ground);
+			}
+		} 
+
+		if(listener.isKeyDown(KeyEvent.VK_UP)) {
+			if(jumpCount == 0 && knight.getState() != "crouch" && !jumping ) {
+				jumping = true;
+				jumpCount = 40;
+				jumpVelocity = -20;
+			}
 		}
-		return hitbox;
+		
+		if (listener.isKeyDown(KeyEvent.VK_SPACE)) {
+			if (cooldown == 0) {
+				if (knight.getState() == "crouch") {
+					Weapon weapon = new Weapon(320, knight.getY() + 10, "lance", knight.getDirection());
+					spriteList.add(weapon);
+				} else {
+					Weapon weapon = new Weapon(320, knight.getY(), "lance", knight.getDirection());
+					spriteList.add(weapon);
+				}
+				cooldown = 20;
+				
+			}
+		}
+
+		if (jumping) {
+			if (jumpCount == 0) {
+				knight.updateY(jumpVelocity);
+				jumping = false;
+			} else {
+				jumpCount -= 1;
+				knight.updateY(jumpVelocity);
+				jumpVelocity += 1;
+			}
+		}
+		
+		// Update all sprites/ground
+
+		for (int i = 0; i < spriteList.size(); i++) {
+			Sprite currentSprite = spriteList.get(i);
+			//update non-structure sprites here
+			currentSprite.update(updates / 6);
+			//remove necessary non-structure sprites
+			if (currentSprite.isRemove()) {
+				spriteList.remove(i);
+			}
+		}	
+		
+		for (int j = 0; j < groundArr.size(); j++) {
+			Sprite currentGround = groundArr.get(j);
+			//update structure sprites here
+			currentGround.update(updates / 6);
+			//remove necessary non-structure sprites
+			if (currentGround.isRemove()) {
+				groundArr.remove(j);
+			}
+		}
+		
+		if (cooldown != 0) {
+			cooldown -= 1;
+		}
+		
+		checkCollisions(spriteList);
+		
+		if(knight.gameOver() == true) {
+			isRunning = false;
+		}
+		
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+
+		g2.fillRect(0, 0, 736, 758);
+		g2.drawImage(backgroundImg, (int) -distance/8, 150, 400 , 300, null);
+		g2.drawImage(scoreImg, 0, 0, 100 , 100, null);
+		//g.drawImage(backBuffer, 0, 0, this);
+
+
+		// draw ground first
+		for (int j = 0; j < groundArr.size(); j++) {
+			groundArr.get(j).draw(g2);
+		}
+		// draw all other sprites
+		for(int i = 0; i < spriteList.size(); i ++) {
+			spriteList.get(i).draw(g2);
+		}
+		
+		// draw all other text
+		
+	}
+
+	public void start() {
+		boolean start = false;
+		initialize();
+		int wait = 0;
+		
+		repaint();
+		while(!start) {
+			wait += 1;
+			System.out.print(""); // Weird bug if this is deleted
+			if (listener.isKeyDown(KeyEvent.VK_SPACE)) {
+				start = true;
+				try {
+					startSound.stop();
+					playSound("GAMEMUSIC.wav");
+				} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		run();
 	}
 	
-	public void playSound(String soundFile) throws MalformedURLException, UnsupportedAudioFileException, IOException, LineUnavailableException {
+	public Clip playSound(String soundFile) throws MalformedURLException, UnsupportedAudioFileException, IOException, LineUnavailableException {
 	    File f = new File("./" + soundFile);
 	    AudioInputStream audioIn = AudioSystem.getAudioInputStream(f.toURI().toURL());  
 	    Clip clip = AudioSystem.getClip();
 	    clip.open(audioIn);
 	    clip.start();
+	    return clip;
+	    
 	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-	
 }
