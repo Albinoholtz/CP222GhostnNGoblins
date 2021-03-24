e1652d41983cf7a0807241ad3df2e424e08cc4a3
Albinoholtz <75751245+Albinoholtz@users.noreply.github.com>
2021-03-23 18:22:04 -0600

    Update Zombie.java

--- Zombie.java
+++ Zombie.java
@@ -1,19 +1,51 @@
-package GhostAndGoblins;
+import java.awt.image.BufferedImage;
 
 /**
  * @author William & Quinn
  *
  */
 public class Zombie extends Enemy {
-
+	
+	BufferedImage[] spawnImg = new BufferedImage[3];
+	BufferedImage[] walkImg = new BufferedImage[3];
+	BufferedImage currentImage = null;
+	String state = null;
+	double spawnCount = -0.5;
+	int velocity = 0;
+	
 	/**
 	 * @param path
 	 * @param _x
 	 * @param _y
 	 */
-	public Zombie(String path, int _x, int _y) {
-		super(path, _x, _y);
-		// TODO Auto-generated constructor stub
+	public Zombie(int _x, int _y) {
+		super(_x, _y);
+		
+		BufferedImage spriteSheet = getImageFile("enemies.png"); // load file
+
+		BufferedImage[] allImg = new BufferedImage[6]; // create list for cutting images
+
+
+		for (int i = 0; i < 5; i++) { // cut images and add to master list
+			if (i < 3) {
+				allImg[i] = spriteSheet.getSubimage(i * 22, 66, 24, 32);
+			} else {
+			allImg[i] = spriteSheet.getSubimage(i * 24, 66, 24, 32); 
+			}
+		}
+		
+		for (int i = 0; i < allImg.length; i++) { // add images to proper lists
+			if (i < 3) {
+				walkImg[i] = allImg[i];
+			} else {
+				spawnImg[i - 3] = allImg[i];
+			}
+		}
+		
+		state = "spawing";
+		currentImage = spawnImg[2];
+		setImage(currentImage);
+
 	}
 
 	@Override
@@ -23,9 +55,36 @@ public class Zombie extends Enemy {
 	}
 
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-
+	public void update(int time) {
+		
+		switch (state) {
+		case "spawing":
+			spawnCount += 0.1;
+			
+			if ((int) (spawnCount / 4) >= 3) {
+				state = "walking";
+				setImage(walkImg[time % 3]);
+				if (getX() < 320) { // set starting direction
+					velocity = 4;
+				} else {
+					velocity = -4;
+				}
+				break;
+			}
+			setImage(spawnImg[2 - (int) (spawnCount / 4)]);
+			break;
+		case "walking":
+			setImage(walkImg[time % 3]);
+			if (getX() < -10) { // switch direction
+				velocity = 4;
+			} else if (getX() > 680){ // switch direction
+				velocity = -4;
+			}
+			
+			updateX(velocity);
+			
+			break;
+			
+		}		
 	}
-
 }
