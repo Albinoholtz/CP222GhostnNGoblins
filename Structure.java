072e1cbf2b846be70a84e4f9887ea0c6d9b96bdc
Albinoholtz <75751245+Albinoholtz@users.noreply.github.com>
2021-03-23 23:08:25 -0600

    Update Structure.java

--- Structure.java
+++ Structure.java
@@ -1,69 +1,139 @@
+
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 /**
  * @author William & Quinn
- * This class create a structure in the world (wall or platform)
+ *
  */
-public class Structure extends Sprite {
+public class Weapon extends Sprite {
+	String direction = null;
+	String weaponType = "lance";
+	boolean hit = false;
+	double hitCount = -0.5;
+	int velocity = 0;
 	
-	BufferedImage currentImage;
-	BufferedImage[] groundImgs = new BufferedImage[3];
-
+	BufferedImage[] lanceImg = new BufferedImage[1];
+	BufferedImage[] torchImg = new BufferedImage[2];
+	BufferedImage[] daggerImg = new BufferedImage[1];
+	BufferedImage[] crossImg = new BufferedImage[1];
+	BufferedImage[] fireImg = new BufferedImage[8];
+	BufferedImage[] hitImg = new BufferedImage[3];
+	
 	/**
-	 * @param path
 	 * @param _x
 	 * @param _y
 	 */
-	public Structure(int _x, int _y) {
+	public Weapon(int _x, int _y, String _type, String _direction) {
 		super(_x, _y);
+		direction = _direction;
+		weaponType = _type;
 		
-		Image groundOne = (Image) getImageFile("foregroundOne.png");
-		Image groundTwo = (Image) getImageFile("foregroundTwo.png");
-		Image groundThree = (Image) getImageFile("foregroundThree.png");
+		Image spriteSheetImg = (Image) getImageFile("knight.png");
 		
-		BufferedImage imageOne = toBufferedImage(groundOne);
-		BufferedImage imageTwo = toBufferedImage(groundTwo);
-		BufferedImage imageThree = toBufferedImage(groundThree);
+		Color backPink = new Color(252,105,216,255);
+		Color backBlue = new Color(117,146,252,255);
+		Color backGreen = new Color(167,226,110,255);
+		Color backCyan = new Color(95,205,228,255);
 		
-		groundImgs[0] = imageOne;
-		groundImgs[1] = imageTwo;
-		groundImgs[2] = imageThree;
+		spriteSheetImg = Transparency.makeColorTransparent(spriteSheetImg, backBlue);
+		spriteSheetImg = Transparency.makeColorTransparent(spriteSheetImg, backPink);
+		spriteSheetImg = Transparency.makeColorTransparent(spriteSheetImg, backGreen);
+		spriteSheetImg = Transparency.makeColorTransparent(spriteSheetImg, backCyan);
 		
-		int max = 10;
-		int min = 0;
-		int random_int = (int) (Math.random() * (max - min + 1) + min);
+		BufferedImage spriteSheet = toBufferedImage(spriteSheetImg);
 		
-		if(random_int >= 8) {
-			this.currentImage = groundImgs[0];
-		} else if (random_int >= 6 && random_int < 8) {
-			this.currentImage = groundImgs[2];
+		lanceImg[0] = spriteSheet.getSubimage(32, 304, 32, 16);
+		torchImg[0] = spriteSheet.getSubimage(0, 288, 16, 16);
+		torchImg[1] = spriteSheet.getSubimage(16, 288, 16, 16);
+		daggerImg[0] = spriteSheet.getSubimage(32, 288, 16, 16);
+		crossImg[0] = spriteSheet.getSubimage(64, 288, 32, 32);
+		// Fire animation
+		fireImg[0] = spriteSheet.getSubimage(192, 160, 32, 32);
+		fireImg[1] = spriteSheet.getSubimage(224, 160, 32, 32);
+		fireImg[2] = spriteSheet.getSubimage(192, 160, 32, 32);
+		fireImg[3] = spriteSheet.getSubimage(224, 160, 32, 32);
+		fireImg[4] = spriteSheet.getSubimage(192, 160, 32, 32);
+		fireImg[5] = spriteSheet.getSubimage(208, 160, 32, 32);
+		fireImg[6] = spriteSheet.getSubimage(192, 160, 32, 32);
+		fireImg[7] = spriteSheet.getSubimage(208, 160, 32, 32);
+		// Weapon hit animation
+		hitImg[0] = spriteSheet.getSubimage(224, 192, 16, 16);
+		hitImg[1] = spriteSheet.getSubimage(240, 192, 16, 16);
+		hitImg[2] = spriteSheet.getSubimage(224, 208, 16, 16);
+		
+		if (direction == "right") {
+			velocity = 13;
 		} else {
-			this.currentImage = groundImgs[1];
+			velocity = -13;
 		}
 		
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean collidedWith(Sprite other) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
-	@Override
-	public void draw(Graphics2D g2) {
-		 g2.drawImage(currentImage, getX(), getY(), 46, 180, null);
+		boolean collided = false;
+		if (overlaps(other)) {
+			if (other instanceof Enemy) { // After collision with enemy, hit & disappear
+				hit = true;
+			}
+		}
+		return collided;
 	}
 
 	@Override
 	public void update(int time) {
 		if(getX() < -92) {
 			setRemove(true);
-		}
-		if(getX() > 828) {
+		} else if(getX() > 828) {
 			setRemove(true);
 		}
+		
+		switch (weaponType) {
+		case "lance":
+			if (hit) {
+				hitCount += 0.2;
+				if ((int) (hitCount) >= 3) {
+					setRemove(true);
+					break;
+				}
+				setImage(hitImg[(int) (hitCount)]);
+				break;
+			}
+			setImage(lanceImg[0]);
+			updateX(velocity);
+			break;
+		}
+	}
+	
+	void draw(Graphics2D g2) {
+		// scales and draws the image by 2
+		if(direction == "right") {
+			g2.drawImage(getImage(), getX(), getY(), getImage().getWidth() * 3, getImage().getHeight() * 3, null);
+		} else {
+			g2.drawImage(getImage(), getX() + 96, getY(), -(getImage().getWidth() * 3), getImage().getHeight() * 3, null);
+		} 
+
+	}
+	
+	@Override
+	public Rectangle getHitbox() {
+		Rectangle hitbox = new Rectangle();
+		switch(weaponType) {
+		default: // default is lance
+			if (direction == "right") {
+				hitbox.setSize(19, 6);
+				hitbox.setLocation(getX() + 10, getY() + 10);
+				break;
+			} else {
+				hitbox.setSize(19, 6);
+				hitbox.setLocation(getX(), getY() + 10);
+				break;	
+			}
+		}
+		return hitbox;
 	}
 }
